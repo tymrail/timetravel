6fde8854dd1a44123bd84a0e521c7e96b36cc3e3
TymRail <tymrail@163.com>
2016-12-13 05:16:24 +0800

    add blog

--- TimeTravelVer1_3/authonline/admin.py
+++ TimeTravelVer1_3/authonline/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from authonline.models import City, Attraction, Route, MyUser, RouteRelation, Team, TeamRelation
+from blog.models import Blog
 
 admin.site.register(City)
 admin.site.register(Attraction)
@@ -8,3 +9,4 @@ admin.site.register(MyUser)
 admin.site.register(RouteRelation)
 admin.site.register(Team)
 admin.site.register(TeamRelation)
+admin.site.register(Blog)
